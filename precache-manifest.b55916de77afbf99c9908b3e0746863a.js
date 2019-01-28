7cc5582f8f1a48801cf720d98e681dc8a71008ab
Arnesh Regmi <arneshregmi@Arneshs-MacBook-Pro.local>
2019-01-28 09:52:12 -0800

    Updates

---
+++ precache-manifest.b55916de77afbf99c9908b3e0746863a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5fc6870ec1d54ae4c3e",
+    "url": "/kunfu2/static/js/main.b5fc6870.chunk.js"
+  },
+  {
+    "revision": "a281d9ca0c02188711c6",
+    "url": "/kunfu2/static/js/1.a281d9ca.chunk.js"
+  },
+  {
+    "revision": "065efe5ca46b50f45f74",
+    "url": "/kunfu2/static/js/runtime~main.065efe5c.js"
+  },
+  {
+    "revision": "0789822387148b4dde0aa8944a5386d2",
+    "url": "/kunfu2/index.html"
+  }
+];
